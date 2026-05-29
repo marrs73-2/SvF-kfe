1aa135013347d2e288906dbea17356f3cf334639
marrs73-2 <marra73@ya.ru>
2026-05-29 21:04:04 +0300

    minor changes to local repo

--- SvFlib/runSvFlocal.sh
+++ SvFlib/runSvFlocal.sh
@@ -2,5 +2,5 @@
 python --version
 pyomo --version
 echo $1
-echo  "python $HOME/git_work/SvFlib/_START.py"
-python $HOME/git_work/SvF/SvFlib/_START.py $1
+echo  "python $HOME/codingIttp/MySvF/SvF-kfe/SvFlib/_START.py"
+python $HOME/codingIttp/MySvF/SvF-kfe/SvFlib/_START.py $1
